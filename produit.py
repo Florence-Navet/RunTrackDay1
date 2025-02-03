35f732538b4342d4a05f596b7dc5142fee56abfb
enolaworld <enolaworld@gmail.com>
2025-02-03 18:28:10 +0100

    job9 fin

--- produit.py
+++ produit.py
@@ -29,10 +29,12 @@ class Produit:
 # Création de plusieurs produits
 produit1 = Produit("Commode", 150, 20)
 produit2 = Produit("Bureau", 120, 20)
+produit3 = Produit("Téléphone", 380, 20)
 
 # Affichage des informations des produits
 print(produit1.afficher())
 print(produit2.afficher())
+print(produit3.afficher())
 
 # Modification du nom et du prix des produits
 produit1.modifier_nom("Lit")
@@ -41,6 +43,10 @@ produit1.modifier_prix(400)
 produit2.modifier_nom("Café")
 produit2.modifier_prix(1.5)
 
+produit3.modifier_nom("voiture")
+produit3.modifier_prix(5000)
+
 # Affichage des nouvelles informations des produits
 print(produit1.afficher())
 print(produit2.afficher())
+print(produit3.afficher())
