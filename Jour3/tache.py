c1ffa2215111dbe20bc7135ac23a662f1857026f
AeRiaSaito <florence.navet@laplateforme.io>
2025-02-05 16:41:46 +0100

    methode marquer_comme_terminée

--- Jour3/tache.py
+++ Jour3/tache.py
@@ -4,6 +4,9 @@ class Tache:
         self.description = description
         self.statut = "à faire"
 
+    def marquer_comme_terminee(self):
+        self.statut = "terminée"
+
     def __str__(self):
         return f'Titre: {self.titre}, Description: {self.description}, Statut: {self.statut}'
 
@@ -19,26 +22,25 @@ class ListeDeTaches:
     def supprimer_tache(self, tache):
         if tache in self.taches:
             self.taches.remove(tache)
-            print(f"La tâche '{tache.titre}' est terminée.")
+            print(f"La tâche '{tache.titre}' est supprimée.")
         else:
             print("La tâche n'existe pas dans la liste.")
 
-    def afficher_liste(self):
+    def marquer_comme_terminee(self, tache):
+        if tache in self.taches:
+            tache.marquer_comme_terminee()
+            print(f"La tâche '{tache.titre}' est marquée comme terminée.")
+        else:
+            print("La tâche n'existe pas dans la liste.")
+
+    def afficher_liste_taches(self):
         if not self.taches:
             print("Aucune tâche dans la liste.")
         else:
             for tache in self.taches:
-                print(tache)
-                print("-" * 20)
+                statut = "[x]" if tache.statut == "terminée" else "[ ]"
+                print(f"{statut} {tache.titre} → {tache.description}")
 
-    def afficher_liste_taches(self):
-        affichage_string = ""
-        for tache in self.taches:
-            if tache.statut == "à faire":
-                affichage_string += "[ ] " + tache.statut.upper() + " : " + tache.titre + " → " + tache.description + "\n"
-            else:
-                affichage_string += "[x] " + tache.statut.upper() + " : " + tache.titre + " → " + tache.description + "\n"
-        return affichage_string
 
 
 # Création de la liste de tâches
@@ -60,7 +62,16 @@ liste.ajouter_tache(tache5)
 
 # Affichage des tâches
 print("Toutes les tâches :")
-liste.afficher_liste()
+liste.afficher_liste_taches()
+print("")
 
-# Affichage personnalisé des tâches
-print(liste.afficher_liste_taches())
+
+
+# Marquer la première tâche comme terminée
+tache1.marquer_comme_terminee()
+
+# Afficher la liste des tâches
+liste.afficher_liste_taches()
+
+
+
