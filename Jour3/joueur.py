fd9ad089c84f269be2997762b2bd6762daabe383
enolaworld <enolaworld@gmail.com>
2025-02-05 19:07:49 +0100

    class joueur

---
+++ Jour3/joueur.py
@@ -0,0 +1,14 @@
+class Joueur:
+        def __init__(self, nom, numero, position, buts_marqués, passes_decisives, cartons_jaunes, cartons_rouges):
+                self.nom = nom
+                self.numero = numero
+                self.position = position
+                self.passes_decisives = passes_decisives
+                self.buts_marqués = buts_marqués
+                self.cartons_jaunes = cartons_jaunes
+                self.cartons_rouges = cartons_rouges
+
+
+class equipe_de_foot:
+        def __init__(self):
+                
