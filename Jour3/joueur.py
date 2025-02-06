af1685b4eff1553592ff9b6c8da3c3e7e66cd368
AeRiaSaito <florence.navet@laplateforme.io>
2025-02-06 10:22:01 +0100

    siimulation match

--- Jour3/joueur.py
+++ Jour3/joueur.py
@@ -27,15 +27,8 @@ class Joueur:
         print(f"Buts marqués: {self.buts_marqués}, Passes décisives: {self.passes_decisives}")
         print(f"Cartons jaunes: {self.cartons_jaunes}, Cartons rouges: {self.cartons_rouges}")
 
-
     def __str__(self):
         return f"{self.nom_joueur} (#{self.numero}, {self.position})"
-
-
-
-
-
-
 
 class EquipeDeFoot:
         def __init__(self, nom_equipe, liste_joueurs = None):
@@ -47,13 +40,19 @@ class EquipeDeFoot:
                        self.liste_joueurs.append(nom_joueur)
                        print(f"Le joueur {nom_joueur} a été selectionné dans l'équipe {self.nom_equipe}")
                 else:
-                        print("Ce joueur est déjà dans l'équipe !!")    
+                        print("Ce joueur est déjà dans l'équipe !!")  
+
+        def afficher_statisques_des_joueurs(self):
+             print(f"Statistiques des joueurs de l'équipe {self.nom_equipe} :") 
+             for joueur in self.liste_joueurs:
+                  f"{joueur.afficher_statistiquesJoueur()}\n"
+                #   print("" * 4)
 
       
                 
          
 
-     			# Création de joueurs
+# Création de joueurs
 joueur1 = Joueur("Kevin Mousquet", 9, "Gardien")
 joueur2 = Joueur("Quentin Mousquet", 7, "Attaquant")
 joueur3 = Joueur("Julien Mousquet", 5, "Défenseur")           
@@ -67,4 +66,13 @@ equipe.ajouter_joueur(joueur4)
 
 
    # Affichage initial des statistiques des joueurs
-joueur1.afficher_statistiquesJoueur()             
+joueur1.afficher_statistiquesJoueur()  
+
+#simuler un match
+joueur1.recevoir_un_carton_jaune()
+joueur4.effectuer_une_passe_decisive()
+joueur3.effectuer_une_passe_decisive()
+joueur2.marquer_but()
+
+# #affichages statistiques des joueurs
+equipe.afficher_statisques_des_joueurs()
---
+++ Jour4/animal.py
@@ -0,0 +1,9 @@
+class Animal:
+    def __init__(self, nom):
+        self.nom = nom
+        
+    def nb_auteur(self):
+        return self.nom
+    
+    def seDeplacer(self):
+        
