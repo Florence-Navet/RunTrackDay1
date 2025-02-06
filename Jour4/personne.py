5fc8011e7e1808a69e12fc57ef17027df5bd2ace
enolaworld <enolaworld@gmail.com>
2025-02-06 18:56:42 +0100

    methode enseigner

--- Jour4/personne.py
+++ Jour4/personne.py
@@ -1,47 +1,53 @@
 class Personne:
     def __init__(self, age=14):
-        self.age = age  
-        
-    def afficherAge(self):
-        print(f"Âge : {self.age}")  
+        self.age = age
+
+    def afficher_age(self):
+        print(f"Âge : {self.age}")
 
     def bonjour(self):
-        print("Hello")  
+        print("Hello")
 
-    def modifierAge(self, nouvel_age):
-        self.age = nouvel_age  
+    def modifier_age(self, nouvel_age):
+        self.age = nouvel_age
 
+class Eleve(Personne):
+    def aller_en_cours(self):
+        print("Je vais en cours")
 
-class Eleve(Personne):  
-    def allerEnCours(self):
-        self.bonjour()  
-        print("Je vais en cours")  
-
-    def afficherAge(self):
-        print(f"J’ai {self.age} ans")  
-
+    def afficher_age(self):
+        print(f"J'ai {self.age} ans")
 
 class Professeur(Personne):
-    def __init__(self, age, matiereEnseignee):
-        super().__init__(age) 
-        self.__matiereEnseignee = matiereEnseignee  
+    def __init__(self, age=14, matiere_enseignee=""):
+        super().__init__(age)
+        self.__matiere_enseignee = matiere_enseignee
 
     def enseigner(self):
-        self.afficherAge() 
-        print("Le cours va commencer") 
+        print("Le cours va commencer")
 
-
-# Instanciation d'une Personne, d'un Élève et d'un Professeur
+# Instanciation d'une Personne et d'un Élève
 personne1 = Personne()
 eleve1 = Eleve()
-professeur1 = Professeur(40, "Mathématiques")  
+
+#objet professeur
+profMaths = Professeur("Kim Tae Young", 40)
+
+#eleve disant bonjour
+eleve1.bonjour()
+
+#elve disant je vais en cours
+eleve1.aller_en_cours()
 
 # Affichage de l'âge par défaut de l'élève
-eleve1.afficherAge()  
+eleve1.afficher_age()
 
-# L'élève va en cours
-eleve1.allerEnCours()  
+#redefinir l'age
+eleve1.modifier_age(15)
+eleve1.afficher_age()
 
-# Le professeur enseigne
-professeur1.enseigner()  
+#professeur dis  bonjour
+profMaths.bonjour()
 
+#commencer le cours
+profMaths.enseigner()
