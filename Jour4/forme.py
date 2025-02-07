afd202da6f4205bb38e94c82bef5f08f21d05d7f
enolaworld <enolaworld@gmail.com>
2025-02-07 12:43:36 +0100

    forme creation objet

--- Jour4/forme.py
+++ Jour4/forme.py
@@ -13,11 +13,20 @@ class Rectangle(Forme):
         return self.longueur * self.largeur
     
     
+class Cercle(Forme):
+    def __init__(self, radius):
+        self.radius = radius
+        
+    def aire(self):
+        return 3.14 * self.radius ** 2
+    
+    
     
 
 #creation d'un rectangle
 rectangle = Rectangle(10, 20)
+cercle = Cercle(10)
 
 #Affichage aire du rectangle
 print(f"L'aire du rectangle est {rectangle.aire()}")
-    
+print(f"L'aire du cercle est : {cercle.aire():.2f}")
